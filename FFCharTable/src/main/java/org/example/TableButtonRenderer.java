1cbeded751af06d312b7a982ee57a26c96bf4951
Lauren Adams <lauren_adams3@baylor.edu>
2022-03-26 18:11:52 -0500

    Semi-usable Table for demo

---
+++ FFCharTable/src/main/java/org/example/TableButtonRenderer.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import javax.swing.*;
+import javax.swing.table.TableCellRenderer;
+import java.awt.*;
+
+public class TableButtonRenderer implements TableCellRenderer {
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column){
+        JButton button = (JButton)value;
+        return button;
+    }
+}
