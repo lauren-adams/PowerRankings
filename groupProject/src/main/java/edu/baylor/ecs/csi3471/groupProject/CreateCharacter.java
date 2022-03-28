abb41a8f438b887b6937fcccb5a40439f4e4b9c8
Jack Hale <jackhale@gmail.com>
2022-03-28 14:57:19 -0500

    added more character creation functionality

--- groupProject/src/main/java/edu/baylor/ecs/csi3471/groupProject/CreateCharacter.java
+++ groupProject/src/main/java/edu/baylor/ecs/csi3471/groupProject/CreateCharacter.java
@@ -1,24 +1,81 @@
 package edu.baylor.ecs.csi3471.groupProject;
 
+import java.awt.GridLayout;
+
 import javax.swing.*;
 
 public class CreateCharacter {
 	protected void createAndShowGUI() {
-		//declaration of variables
+		/*//declaration of variables
+		GridLayout g = new GridLayout(4, 3);
 		JFrame createFrame;
-		JPanel createPanel;
-		
+		JLabel nameLabel, descLabel, urlLabel;
+		JTextField nameField, descField, urlField;
 		
 		//initialization of variables
 		createFrame = new JFrame("Create Character");
-		createPanel = new JPanel();
+		
+		nameLabel = new JLabel("Name:");
+		nameField = new JTextField();
+		//nameField.setColumns(30);
+		
+		descLabel = new JLabel("Description:");
+		descField = new JTextField();
+		//descField.setColumns(30);
+		
+		urlLabel = new JLabel("Photo URL:");
+		urlField = new JTextField();
+		//urlField.setColumns(30);
 		
 		//adding elements and listeners
+		createFrame.add(nameLabel);
+		createFrame.add(nameField);
 		
+		createFrame.add(descLabel);
+		createFrame.add(descField);
+		
+		createFrame.add(urlLabel);
+		createFrame.add(urlField);
 		
 		//setting visible
+		createFrame.setLayout(g);
+		createFrame.pack();
+		createFrame.setVisible(true);*/
+		
+		//declaration of variables
+		GridLayout g = new GridLayout(4, 3);
+		JFrame createFrame;
+		JLabel nameLabel, descLabel, urlLabel;
+		JTextField nameField, descField, urlField;
+		
+		//initialization of variables
+		createFrame = new JFrame("Create Character");
+		
+		nameLabel = new JLabel("Name:");
+		nameField = new JTextField();
+		//nameField.setColumns(30);
+		
+		descLabel = new JLabel("Description:");
+		descField = new JTextField();
+		//descField.setColumns(30);
+		
+		urlLabel = new JLabel("Photo URL:");
+		urlField = new JTextField();
+		//urlField.setColumns(30);
+		
+		//adding elements and listeners
+		createFrame.add(nameLabel);
+		createFrame.add(nameField);
+		
+		//createFrame.add(descLabel);
+		//createFrame.add(descField);
+		
+		//createFrame.add(urlLabel);
+		//createFrame.add(urlField);
+		
+		//setting visible
+		//createFrame.setLayout(g);
+		createFrame.pack();
 		createFrame.setVisible(true);
-		
-		
 	}
 }
