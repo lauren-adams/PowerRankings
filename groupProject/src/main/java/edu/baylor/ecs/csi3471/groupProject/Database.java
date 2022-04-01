2266e6b6b757c1f88c7c777e15c72048cb0ec2ca
Lauren Adams <lauren_adams3@baylor.edu>
2022-03-31 19:57:00 -0500

    Fixed LoginDAO

--- groupProject/src/main/java/edu/baylor/ecs/csi3471/groupProject/Database.java
+++ groupProject/src/main/java/edu/baylor/ecs/csi3471/groupProject/Database.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class Database {
+public class loginDAO {
 	public static int userLine;
 	
 	protected boolean isUsernameTaken(String username){
