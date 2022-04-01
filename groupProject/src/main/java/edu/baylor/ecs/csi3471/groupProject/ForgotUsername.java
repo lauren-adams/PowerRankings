bf7407c24d77c7e1a1307293d93fa3bf76683748
Jack Hale <jackhale@gmail.com>
2022-03-31 21:27:20 -0500

    forgot adaptations

--- groupProject/src/main/java/edu/baylor/ecs/csi3471/groupProject/ForgotUsername.java
+++ groupProject/src/main/java/edu/baylor/ecs/csi3471/groupProject/ForgotUsername.java
@@ -69,14 +69,6 @@ public class ForgotUsername extends JPanel {
         frame.pack();
         frame.setVisible(true);
     }
-//    public static void main(String[] args) {
-//        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-//            @Override
-//            public void run() {
-//                createAndShowGUI();
-//            }
-//        });
-//    }
 
 }
 
