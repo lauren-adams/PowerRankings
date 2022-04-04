6c8da870979e118188bf1d73fbf4730a3b99bae9
Jack Hale <jackhale@gmail.com>
2022-04-04 17:24:21 -0500

    removed unnecessary comments

--- groupProject/src/main/java/edu/baylor/ecs/csi3471/groupProject/Business/Runner.java
+++ groupProject/src/main/java/edu/baylor/ecs/csi3471/groupProject/Business/Runner.java
@@ -5,8 +5,6 @@ import edu.baylor.ecs.csi3471.groupProject.UI.LoginPage;
 public class Runner {
 	public static User curUser;
 	public static void main(String[] args) {
-//		HomePage h = new HomePage();
-//		h.createAndShowGUI();
 		LoginPage lp = new LoginPage();
 	}
 }
