69c85d6c7b9a5557aedb0a8bc0ea83abb00e1b85
cypill36 <cyril_pillai1@baylor.edu>
2022-04-30 14:30:20 -0500

    Tournament Bracket updates

--- groupProject/src/main/java/edu/baylor/ecs/csi3471/groupProject/UI/TournamentBracketPanel.java
+++ groupProject/src/main/java/edu/baylor/ecs/csi3471/groupProject/UI/TournamentBracketPanel.java
@@ -2,6 +2,7 @@ package edu.baylor.ecs.csi3471.groupProject.UI;
 
 
 import edu.baylor.ecs.csi3471.groupProject.Business.Character;
+import edu.baylor.ecs.csi3471.groupProject.Business.Runner;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
@@ -10,6 +11,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.net.URL;
+import java.util.logging.Logger;
 
 
 class ImagePanel extends JPanel {
@@ -55,9 +57,10 @@ class ImagePanel extends JPanel {
 
 public class TournamentBracketPanel
 {
-
+    //public static Logger logger = Logger.getLogger(Runner.class.getName());
     public Character[] initTournamentCharacters()
     {
+        Runner.logger.info("Initializing tournament characters");
         Character[] characters = new Character[15];
         BufferedReader reader = null;
         try
@@ -75,7 +78,7 @@ public class TournamentBracketPanel
         }
         catch(Exception e)
         {
-            System.out.println(e);
+            Runner.logger.severe("unable to read characters from characterRounds.cdv file");
             System.exit(1);
         }
         return characters;
@@ -84,6 +87,7 @@ public class TournamentBracketPanel
 
     public Character[] getBracketCharacters()
     {
+        Runner.logger.info("returning tournament characters");
         Character[] myChars = initTournamentCharacters();
         return myChars;
     }
@@ -111,6 +115,7 @@ public class TournamentBracketPanel
                 Image tempImg = ImageIO.read(url);
                 tempImgPanel = new ImagePanel(tempImg);
 
+                Runner.logger.info("image " + i + " is being added to the bracket");
                 tempImgPanel.setBounds(xCoord, yCoord, 75, 75);
                 myBigPane.add(tempImgPanel, JLayeredPane.PALETTE_LAYER);
                 yCoord += yInc;
@@ -118,12 +123,13 @@ public class TournamentBracketPanel
         }
         catch(Exception e)
         {
-            System.out.println(e);
+            Runner.logger.severe("could not load the images onto the tournament bracket");
             System.exit(1);
         }
 
         return myBigPane;
     }
+
 
 
     public JLayeredPane getBracket()
@@ -135,6 +141,7 @@ public class TournamentBracketPanel
 //        frame.setLocationRelativeTo(null);
 
 
+        Runner.logger.info("tournament bracket being generated");
         ImagePanel imgPanel = null;
         try
         {
@@ -155,9 +162,16 @@ public class TournamentBracketPanel
 
         myBigPane.add(imgPanel, JLayeredPane.DEFAULT_LAYER);
 
+
+        Runner.logger.info("adding round one to the tournament bracket");
         myBigPane = addImagesToBracket(myChars, myBigPane, 0, 125, 450, 175, 0, 8);
-        myBigPane = addImagesToBracket(myChars, myBigPane, 175, 230, 120, 350, 8, 12);
-        myBigPane = addImagesToBracket(myChars, myBigPane, 175, 420, 120, -80, 12, 14);
+
+
+//        logger.info("adding round two to the tournament bracket");
+//        myBigPane = addImagesToBracket(myChars, myBigPane, 175, 230, 120, 350, 8, 12);
+//
+//        logger.info("adding round three to the tournament bracket");
+//        //myBigPane = addImagesToBracket(myChars, myBigPane, 175, 420, 120, -80, 12, 14);
 
 //        frame.add(myBigPane);
 //        frame.setVisible(true);
@@ -165,12 +179,44 @@ public class TournamentBracketPanel
         return myBigPane;
     }
 
-//    public static void main(String[] args)
-//    {
-//        TournamentBracketFrame tBracket = new TournamentBracketFrame();
-//        JLayeredPane myLayeredPane = tBracket.getBracket();
-//
-//    }
+
+    public JLayeredPane getRound2(JLayeredPane myBigPane)
+    {
+//        JFrame frame = new JFrame("TEMP");
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setSize(550, 550);
+//        //frame size
+//        frame.setLocationRelativeTo(null);
+
+        Character[] myChars = initTournamentCharacters();
+
+        Runner.logger.info("adding round two to the tournament bracket");
+        myBigPane = addImagesToBracket(myChars, myBigPane, 175, 230, 120, 350, 8, 12);
+
+//        frame.add(myBigPane);
+//        frame.setVisible(true);
+
+        return myBigPane;
+    }
+
+    public JLayeredPane getRound3(JLayeredPane myBigPane)
+    {
+//        JFrame frame = new JFrame("TEMP");
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setSize(550, 550);
+//        //frame size
+//        frame.setLocationRelativeTo(null);
+
+        Character[] myChars = initTournamentCharacters();
+
+        Runner.logger.info("adding round three to the tournament bracket");
+        myBigPane = addImagesToBracket(myChars, myBigPane, 175, 420, 120, -80, 12, 14);
+
+//        frame.add(myBigPane);
+//        frame.setVisible(true);
+
+        return myBigPane;
+    }
 }
 
 
